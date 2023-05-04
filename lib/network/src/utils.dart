9923e8f78f2c4a5787b878ca1b4ec4fdeb70ac5d
Thomas Roovers <troovers@e-sites.nl>
2023-05-04 22:46:37 +0200

    Allow keychain access in locked state for API credentials

--- lib/network/src/utils.dart
+++ lib/network/src/utils.dart
@@ -1,3 +1,4 @@
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:illuminate/storage.dart';
 
 const storageKeyAccessToken = "oauth_access_token";
@@ -6,4 +7,7 @@ const storageKeyTokenExpirationDate = "oauth_access_token_expiration_date";
 
 final storageManager = StorageManager(
   prefix: 'oauth',
+  iosConfig: IOSConfig(
+    accessibility: KeychainAccessibility.first_unlock,
+  ),
 );
